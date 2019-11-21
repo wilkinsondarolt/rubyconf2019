95eb5d0272d18dce443015d5af2439bf0da7bd9a
Wilkinson Da Rolt de Souza <wilki.darolt@gmail.com>
2019-11-21 00:01:54 -0300

    Change to create enemies randomly.

--- src/game.rb
+++ src/game.rb
@@ -7,22 +7,35 @@ require_relative 'player'
 class Game < Gosu::Window
   HEIGHT = 400
   WIDTH = 400
+  ENEMY_SPAWN_RATIO = 3
 
   def initialize
     super(WIDTH, HEIGHT)
     self.caption = 'Back to end'
     @player = Player.new(self)
-    @enemy = Enemy.new(self)
+    @enemies = []
   end
 
   def draw
     @player.draw
-    @enemy.draw
+    @enemies.each(&:draw)
   end
 
   def update
+    spawn_enemy if spawn_new_enemy?
+
     @player.update
-    @enemy.update
+    @enemies.each(&:update)
+  end
+
+  private
+
+  def spawn_enemy
+    @enemies << Enemy.new(self)
+  end
+
+  def spawn_new_enemy?
+    rand(1..100) < ENEMY_SPAWN_RATIO
   end
 end
 
